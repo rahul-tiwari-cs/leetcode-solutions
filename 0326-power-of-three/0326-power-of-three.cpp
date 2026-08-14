0854232380c69b5bb88f036e85db0c45d8e75411
Rahul Tiwari <rahultiwari17153@gmail.com>
2026-08-15 01:26:27 +0530

    Time: 3 ms (59.1%), Space: 8.9 MB (44.05%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-       if (!isMultipleOfThree(n)) return (n == 1);
-       return isPowerOfThree(n / 3);
-    }
-private:
-    bool isMultipleOfThree(int n){
-        int sod = 0;  //sum of digits
-        while (n > 0){
-            sod += (n % 10);
-            n /= 10;
-        }
-        return (sod % 3 == 0 && sod !=0);
+        if (n == 1 || n <= 0 || (n % 3) != 0) return (n == 1);
+        return isPowerOfThree(n / 3);
     }
 };
