81727fee8133f0e43a5caa92927c362d5a790d97
Rahul Tiwari <rahultiwari17153@gmail.com>
2026-08-20 23:32:43 +0530

    Time: 0 ms (100%), Space: 26.3 MB (36.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,7 +5,7 @@ public:
         if (n == 1 || nums[0] != nums[1]) return nums[0];
         if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
         int l = 1;
-        int r = n - 1;
+        int r = n - 2;
         while (l <= r){
             int mid = l + (r - l)/2;
             if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) return nums[mid];
