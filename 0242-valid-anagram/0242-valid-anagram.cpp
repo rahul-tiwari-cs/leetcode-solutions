fe158016c74bb424e5ce44cfe4061910b01d2736
Rahul Tiwari <rahultiwari17153@gmail.com>
2026-09-10 19:08:45 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.64%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,11 +4,7 @@ public:
         if (s.size() != t.size()) return false;
         unordered_map<char, int> mpp;
         for (int i = 0; i < s.size(); i++){
-            if(mpp.find(s[i]) != mpp.end()){
-                mpp[s[i]] += 1;
-            } else {
-                mpp[s[i]] = 1;
-            }
+            mpp[s[i]]++;
         }
         for (int i = 0; i < t.size(); i++){
             if (mpp.find(t[i]) != mpp.end() && mpp[t[i]] >= 1){
